fd01178397fd51af99b52fb630e7063d90af8998
joshfeds14 <joshfeds14@gmail,com>
2021-06-04 21:27:28 -0400

    Exercise 07

---
+++ src/main/java/oop/Exercise07/Exercise07.java
@@ -0,0 +1,13 @@
+package oop.Exercise07;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Joshua Federman
+ */
+public class Exercise07
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
